edc62246cb5e20f46be39d5b7a6a9b1684714ba5
Adwaith <adwaith@frugalscientific.com>
2025-07-30 18:02:54 +0530

    logger for thunk

--- frontend/Catering_App/src/redux/store.js
+++ frontend/Catering_App/src/redux/store.js
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import vendorReducer from "./slices/vendorSlice";
 import userReducer from "./slices/userSlice";
 import adminReducer from "./slices/adminSlice";
-import loginReducer from './slices/loginSlice'
+import loginReducer from "./slices/loginSlice";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: {
     vendor: vendorReducer,
     admin: adminReducer,
     user: userReducer,
-    login:loginReducer
+    login: loginReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
